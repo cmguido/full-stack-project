638667186fbc9839691de15c71a8232a8994586a
cmguido <chris.m.guido@gmail.com>
2017-09-22 11:32:14 -0400

    Add table notes
    - scaffold table notes, with openreadprotection
    - updated notes controller so only current_user can modify
    - users has_many notes now
    - notes belongs_to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :notes
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: %i[index show]
+  resources :notes, only: %i[index create show update destroy]
 end
--- scripts/notes/create-example.sh
+++ scripts/notes/create-example.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/notes"
+URL_PATH="/examples"
 curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=$TOKEN" \
   --data '{
-    "note": {
-      "comment": "'"${COMMENT}"'",
-      "time": "'"${TIME}"'"
+    "example": {
+      "text": "'"${TEXT}"'"
     }
   }'
 
